b49c27965dcfe478301c8640580e59763a481280
HahaTTpro <ttpro1995@yahoo.com.vn>
2014-12-05 23:48:17 +0700

    add note c++11

--- DinerMenu.h
+++ DinerMenu.h
@@ -7,8 +7,8 @@ using namespace std;
 class DinerMenu
 {
 private:
-	int MAX_ITEMS=6;
-	int  numberOfItems = 0;
+	int MAX_ITEMS=6;//c++11
+	int  numberOfItems = 0;//c++11
 	MenuItem* menu;
 public:
 	DinerMenu();
